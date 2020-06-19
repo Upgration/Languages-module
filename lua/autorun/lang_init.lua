47d20dbd2c277e7161a5dcc3688733b1322a9350
Foks <37844753+FoksVHox@users.noreply.github.com>
2020-06-19 22:55:19 +0200

    Add files via upload

---
+++ lua/autorun/lang_init.lua
@@ -0,0 +1,11 @@
+--[[
+Written with Lua++.
+Don't remove this notice please
+
+https://github.com/LuaPlusPlus/lua-plus-plus
+]]--
+include('lang.lua')
+AddCSLuaFile('lang.lua')
+
+include('promises.lua')
+AddCSLuaFile('promises.lua')
